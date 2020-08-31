be906de31274333068b51f32af310ab7e497596b
Kamil Rogala <rogalak25@gmail.com>
2020-08-31 12:02:34 +0200

    "Zmiany zmiany"

---
+++ src/main/java/com/example/memy/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.memy;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String getHome(ModelMap map){
+        map.put("gifs", Gif.GIFS);
+        return "home";
+    }
+
+}
